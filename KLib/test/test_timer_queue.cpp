506038645f37fe88da83f10074836f36af94a44f
likang <11102224@vivo.xyz>
2019-09-25 20:11:53 +0800

    [UPDATE] 编译测试代码

--- KLib/test/test_timer_queue.cpp
+++ KLib/test/test_timer_queue.cpp
@@ -1,22 +1,20 @@
 #include "utils/TimerTaskQueue.hpp"
 #include <iostream>
 #include <boost/chrono.hpp>
+#include <boost/chrono/chrono_io.hpp>
+#include <boost/chrono.hpp>
 
 static void handler(void)
 {
-    KLib::TimerTaskQueue::timer_type::TimePoint now = boost::chrono::high_resolution_clock::now();    
-    std::cout << "now : " << boost::chrono::duration_cast<KLib::TimerTaskQueue::timer_type::Seconds>(now).count() << std::endl;
+    KLib::TimerTaskQueue::timer_type::TimePoint now = KLib::TimerTaskQueue::timer_type::Clock::now();
+    std::time_t t = KLib::TimerTaskQueue::timer_type::Clock::to_time_t(now);    
+    std::cout << std::ctime(&t) << std::endl;
 }
 
 int main(void)
 {
-    KLib::TimerTaskQueue::timer_type::TimePoint now = boost::chrono::high_resolution_clock::now();
-    std::cout << "now : " << boost::chrono::duration_cast<KLib::TimerTaskQueue::timer_type::Seconds>(now).count() << std::endl;
     KLib::TimerTaskQueue timer_queue;
-    timer_queue.runAfter(handler, KLib::TimerTaskQueue::timer_type::Seconds(2));
-
     timer_queue.runEvery(handler, KLib::TimerTaskQueue::timer_type::Seconds(4));
-
     timer_queue.exec();
     
     return 0;
